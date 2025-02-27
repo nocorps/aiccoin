1b046c9e5de4fe70f935e31f7d9057868a4b42bf
Kanagaraj.M <112673484+KANAGARAJ-M@users.noreply.github.com>
2025-02-27 07:20:58 +0530

    Update ReferralPage.vue

--- src/views/ReferralPage.vue
+++ src/views/ReferralPage.vue
@@ -73,7 +73,7 @@ export default {
 
     // Generate referral link
     const referralLink = computed(() => {
-      return user.value ? `${window.location.origin}/register?ref=${user.value.referralCode}` : '';
+      return user.value ? `https://t.me/AIC_Coin_Bot/AIC/register?ref=${user.value.referralCode}` : '';
     });
 
     // Copy referral link to clipboard
